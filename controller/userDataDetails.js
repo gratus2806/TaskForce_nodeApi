2ef1d4e8c5a47152abdaff3ca351e8845dd5e018
gratus2806 <gratusbaptist2806@gmail.com>
2021-05-07 14:45:15 +0530

    addeded start script

--- controller/userDataDetails.js
+++ controller/userDataDetails.js
@@ -1,24 +1,24 @@
-const AuthenticationDetails = require('../models/userData');
+const userAuth = require('../models/userData');
 
 module.exports.loginctrl = (req, res) => {
     const payload = req.body;
     console.log("payload",payload)
     
-    AuthenticationDetails.find({
+    userAuth.find({
         userPhone:payload.userPhone,
         password:payload.Password 
     })
-    .then((authenticationDetails)=>{
-        if(authenticationDetails!=""){
-            // console.log("authenticationDetails",authenticationDetails)
-            if(authenticationDetails[0].userPhone==payload.userPhone && authenticationDetails[0].password==payload.Password){
+    .then((userAuth)=>{
+        if(userAuth!=""){
+            // console.log("userAuth",userAuth)
+            if(userAuth[0].userPhone==payload.userPhone && userAuth[0].password==payload.Password){
                 var userDetails={
-                    status: authenticationDetails[0].status,
-                    userId: authenticationDetails[0].userId,
-                    userParish: authenticationDetails[0].userParish,
-                    userPhone: authenticationDetails[0].userPhone,
-                    userVillage: authenticationDetails[0].userVillage,
-                    username: authenticationDetails[0].username,
+                    status: userAuth[0].status,
+                    userId: userAuth[0].userId,
+                    userParish: userAuth[0].userParish,
+                    userPhone: userAuth[0].userPhone,
+                    userVillage: userAuth[0].userVillage,
+                    username: userAuth[0].username,
                 }
                 return res.send({status : true,userDetails})
             }  
@@ -33,7 +33,7 @@ module.exports.userReg = (req, res) => {
     const payload = req.body;
     console.log("payload",payload)
     
-    let newRecord = new AuthenticationDetails({
+    let newRecord = new userAuth({
         userId: Math.floor(10000000000 + Math.random() * 90000000000),
         status: '1',
         password:payload.userPassword,
@@ -43,8 +43,8 @@ module.exports.userReg = (req, res) => {
         userPhone:payload.userPhoneNumber,
     })
     return newRecord.save()
-    .then((AuthenticationDetails)=>{
-        if(!!AuthenticationDetails){
+    .then((userAuth)=>{
+        if(!!userAuth){
             return res.send({status : true })
         } else{
             return res.send({status : false })
