b4c3bd57d27625960b5519b2979bf3a2a0c95028
gratus2806 <gratusbaptist2806@gmail.com>
2021-05-07 13:14:48 +0530

    controller set

--- controller/userDataDetails.js
+++ controller/userDataDetails.js
@@ -6,13 +6,21 @@ module.exports.loginctrl = (req, res) => {
     
     AuthenticationDetails.find({
         userPhone:payload.userPhone,
-        password:payload.password 
+        password:payload.Password 
     })
     .then((authenticationDetails)=>{
-        console.log("authenticationDetails",authenticationDetails)
         if(authenticationDetails!=""){
-            if(authenticationDetails[0].userPhone==payload.userPhone && authenticationDetails[0].password==payload.password){
-                return res.send({status : true,authenticationDetails})
+            // console.log("authenticationDetails",authenticationDetails)
+            if(authenticationDetails[0].userPhone==payload.userPhone && authenticationDetails[0].password==payload.Password){
+                var userDetails={
+                    status: authenticationDetails[0].status,
+                    userId: authenticationDetails[0].userId,
+                    userParish: authenticationDetails[0].userParish,
+                    userPhone: authenticationDetails[0].userPhone,
+                    userVillage: authenticationDetails[0].userVillage,
+                    username: authenticationDetails[0].username,
+                }
+                return res.send({status : true,userDetails})
             }  
         } else{
             return res.send({status : false})
@@ -28,11 +36,11 @@ module.exports.userReg = (req, res) => {
     let newRecord = new AuthenticationDetails({
         userId: Math.floor(10000000000 + Math.random() * 90000000000),
         status: '1',
-        password:'gratus2806',
-        username:'Gratus Dbritto',
-        userVillage:'JELADI',
-        userParish:'NANDAKHAL',
-        userPhone:'9762173505'
+        password:payload.userPassword,
+        username:payload.userFullName,
+        userVillage:payload.userVillage,
+        userParish:payload.userParish,
+        userPhone:payload.userPhoneNumber,
     })
     return newRecord.save()
     .then((AuthenticationDetails)=>{
--- routes/userAuth.js
+++ routes/userAuth.js
@@ -7,7 +7,7 @@ const ctrllogin = require('../controller/userDataDetails');
 const ctrlReg = require('../controller/userDataDetails');
 
 
-router.get('/user_details',ctrllogin.loginctrl);
+router.post('/user_details',ctrllogin.loginctrl);
 router.post('/user_details_reg',ctrlReg.userReg);
 
 module.exports = router;
